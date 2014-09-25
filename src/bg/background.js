7d2dca8e164452962a201e88dbb50dfe2d4849c9
Leeroy Brun <leeroy.brun@gmail.com>
2014-09-25 21:55:25 +0200

    Initial commit.

---
+++ src/bg/background.js
@@ -0,0 +1,6 @@
+/*chrome.cookies.onChanged.addListener(function(info) {
+	if(info.cookie.domain.indexOf('shazam.com') != -1) {
+		var popupUrl = chrome.extension.getURL('src/popup/popup.html');
+		focusOrCreateTab(popupUrl);
+	}
+});*/
