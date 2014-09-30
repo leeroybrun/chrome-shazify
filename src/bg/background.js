ea9f33317217006e5a216ae453c0c5b787936cea
Leeroy Brun <leeroy.brun@gmail.com>
2014-09-30 12:51:52 +0200

    Remove background script

--- src/bg/background.js
+++
@@ -1,6 +0,0 @@
-/*chrome.cookies.onChanged.addListener(function(info) {
-	if(info.cookie.domain.indexOf('shazam.com') != -1) {
-		var popupUrl = chrome.extension.getURL('src/popup/popup.html');
-		focusOrCreateTab(popupUrl);
-	}
-});*/
