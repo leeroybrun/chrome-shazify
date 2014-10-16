30d42cc6e2b0ab5d1d94febb7de2d8840dd825ee
Leeroy Brun <leeroy.brun@gmail.com>
2014-10-16 11:40:47 +0200

    Add bundle action to Grunt

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		archive_name: 'shazam2spotify-<%= pkg.version %>',
 		jshint: {
 			globals: { angular: true },
 			all: ['gruntfile.js', 'src/popup/js/**/*.js'],
@@ -47,15 +48,55 @@ module.exports = function(grunt) {
 					spawn: false,
 				}
 			}
-		}
+		},
+
+		clean: {
+            pre: ['dist/', 'build/'],
+            post: ['<%= archive_name %>.zip', 'build/']
+        },
+
+        compress: {
+            main: {
+                options: {
+                    archive: '<%= archive_name %>.zip'
+                },
+                expand: true,
+                cwd: 'build/',
+                src: ['**/*'],
+                dest: ''
+            }
+        },
+
+        copy: {
+            main: {
+                files: [
+                    {expand: true, src: ['_locales/**'], dest: 'build/'},
+                    {expand: true, src: ['icons/**'], dest: 'build/'},
+                    {expand: true, src: ['popup/**'], dest: 'build/'},
+                    {expand: true, src: ['LICENCE'], dest: 'build/'},
+                    {expand: true, src: ['manifest.json'], dest: 'build/'},
+                    {expand: true, src: ['README.md'], dest: 'build/'}
+                ]
+            },
+            archive: {
+                files: [
+                    {expand: true, src: ['<%= archive_name %>.zip'], dest: 'dist/'}
+                ]
+            }
+        },
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	grunt.registerTask('build', ['concat:css', 'jshint', 'concat:js']);
 	grunt.registerTask('build:css', ['concat:css']);
 	grunt.registerTask('build:js', ['jshint', 'concat:js']);
+
+	grunt.registerTask('bundle', ['clean:pre', 'build', 'copy:main', 'compress', 'copy:archive', 'clean:post']);
 
 };
