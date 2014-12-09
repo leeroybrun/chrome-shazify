54181a9a43eddee7a19e8fd7ffb29a37ee8fd4d9
Leeroy Brun <leeroy.brun@gmail.com>
2014-12-09 20:50:26 +0100

    Load SVG icons with $http instead of jQuery

--- src/popup/js/config/svgIcons.js
+++ src/popup/js/config/svgIcons.js
@@ -1,12 +1,8 @@
 angular.module('Shazam2Spotify')
-	.config(function() {
+	.run(function($http) {
 		// Load SVG icons (dirty, should not use jQuery...)
-		$.ajax({
-			url: 'img/icons.svg',
-			method: 'GET',
-			dataType: 'html',
-			success: function(data) {
-				$("body").prepend(data);
-			}
-		});
+		$http.get('img/icons.svg', {responseType: 'html'}).
+			success(function(data) {
+				angular.element('body').prepend(data);
+			});
 	});
