f65095b44e06976f0b35c3cf0dc5ebd232e77306
Leeroy Brun <leeroy.brun@gmail.com>
2014-12-09 19:26:34 +0100

    Exported logs always open in new tab

--- src/popup/js/helpers/ChromeHelper.js
+++ src/popup/js/helpers/ChromeHelper.js
@@ -1,24 +1,44 @@
 angular.module('Shazam2Spotify').factory('ChromeHelper', function() {
 	var ChromeHelper = {
+		findExistingTab: function(url, callback) {
+			chrome.windows.getAll({'populate': true}, function(windows) {
+				var existing_tab = null;
+				for (var i in windows) {
+					var tabs = windows[i].tabs;
+					for (var j in tabs) {
+						var tab = tabs[j];
+						if (tab.url == url) {
+							existing_tab = tab;
+							break;
+						}
+					}
+				}
+
+				callback(existing_tab);
+			});
+		},
+		
+		// Remove existing tab, and recreate it
+		removeAndCreateTab: function(url) {
+			ChromeHelper.findExistingTab(url, function(existing_tab) {
+				if (existing_tab) {
+					chrome.tabs.remove(existing_tab.id);
+				}
+				
+				chrome.tabs.create({'url': url, 'selected': true});
+			});
+		},
+
+		// Focus existing tab or create it
 		focusOrCreateTab: function(url) {
-		  chrome.windows.getAll({"populate":true}, function(windows) {
-		    var existing_tab = null;
-		    for (var i in windows) {
-		      var tabs = windows[i].tabs;
-		      for (var j in tabs) {
-		        var tab = tabs[j];
-		        if (tab.url == url) {
-		          existing_tab = tab;
-		          break;
-		        }
-		      }
-		    }
-		    if (existing_tab) {
-		      chrome.tabs.update(existing_tab.id, {"selected":true});
-		    } else {
-		      chrome.tabs.create({"url":url, "selected":true});
-		    }
-		  });
+			ChromeHelper.findExistingTab(url, function(existing_tab) {
+				if (existing_tab) {
+					chrome.tabs.reload(existing_tab.id, {'bypassCache': true});
+					chrome.tabs.update(existing_tab.id, {'selected': true});
+				} else {
+					chrome.tabs.create({'url': url, 'selected': true});
+				}
+			});
 		},
 
 		exportData: function(fileName, data) {
@@ -36,7 +56,7 @@ angular.module('Shazam2Spotify').factory('ChromeHelper', function() {
 					            return;
 					        }
 
-				            ChromeHelper.focusOrCreateTab(fileEntry.toURL());
+				            ChromeHelper.removeAndCreateTab(fileEntry.toURL());
 				        };
 
 				        fileWriter.onerror = function(error) {
